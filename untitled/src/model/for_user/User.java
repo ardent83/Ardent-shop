67a260323f4c9d803fe848e4ea23b0bf22c9f2f0
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-03-30 00:41:05 +0430

    Encapsulation of user class variables and (abstract).

--- untitled/src/model/for_user/User.java
+++ untitled/src/model/for_user/User.java
@@ -1,8 +1,36 @@
 package model.for_user;
-public class User {
-    String id;
-    String email;
-    String number;
-    String password;
-    UserType userType;
+public abstract class User {
+    public User(String id, String email, String number, String password, UserType userType) {
+        this.id = id;
+        this.email = email;
+        this.number = number;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    private String id;
+    private String email;
+    private String number;
+    private String password;
+    private UserType userType;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
 }
