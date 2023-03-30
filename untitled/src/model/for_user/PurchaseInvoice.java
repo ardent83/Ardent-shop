09ae02f1709bb56f1231e793c3c0cfdb68958533
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-03-31 03:17:35 +0430

    add class PurchaseInvoice

---
+++ untitled/src/model/for_user/PurchaseInvoice.java
@@ -0,0 +1,47 @@
+package model.for_user;
+import model.for_item.Item;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class PurchaseInvoice {
+    public PurchaseInvoice(double amountPaid, ArrayList<Item> itemArrayList) {
+        this.idPurchaseInvoice = Integer.toHexString(countForId+61153);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Date tempDate = new Date();
+        this.date = formatter.format(tempDate);
+        this.amountPaid = amountPaid;
+        this.itemArrayList = new ArrayList<>(itemArrayList);
+    }
+
+    private static int countForId;
+    private String idPurchaseInvoice;
+    private String date;
+    private double amountPaid;
+    private ArrayList<Item> itemArrayList;
+
+    public static int getCountForId() {
+        return countForId;
+    }
+
+    public String getIdPurchaseInvoice() {
+        return idPurchaseInvoice;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+
+    public ArrayList<Item> getItemArrayList() {
+        return itemArrayList;
+    }
+
+    static {
+        countForId = 0;
+    }
+
+}
