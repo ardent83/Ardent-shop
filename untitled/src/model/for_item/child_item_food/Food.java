04c42baf00852584650dcc6f50fdd3f8be592194
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-03-31 05:18:18 +0430

    add class Food in pkg child_item_food

---
+++ untitled/src/model/for_item/child_item_food/Food.java
@@ -0,0 +1,23 @@
+package model.for_item.child_item_food;
+
+import model.for_item.Category;
+import model.for_item.Item;
+
+public class Food extends Item {
+    public Food(String name, double price, int availableNumber, Category category, String productionDate, String expirationDate) {
+        super(name, price, availableNumber, category);
+        this.productionDate = productionDate;
+        this.expirationDate = expirationDate;
+    }
+
+    private final String productionDate;
+    private final String expirationDate;
+
+    public String getProductionDate() {
+        return productionDate;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+}
