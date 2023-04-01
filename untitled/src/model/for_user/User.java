62ea597fd98201f073cda67c4f6e2e3ed9521b1e
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-04-01 13:04:46 +0430

    fix id admin

--- untitled/src/model/for_user/User.java
+++ untitled/src/model/for_user/User.java
@@ -1,18 +1,23 @@
 package model.for_user;
 public abstract class User {
     public User(String email, String number, String password, UserType userType) {
-        this.idUser = Integer.toHexString(countForId+43681);
+        if(userType.equals(UserType.BUYER)){
+            this.idUser = Integer.toHexString(countForId+43681);
+        } else {
+            this.idUser = "admin";
+        }
         this.email = email;
         this.number = number;
         this.password = password;
         this.userType = userType;
+        countForId++;
     }
     private static int countForId;
     private final String idUser;
     protected String email;
     protected String number;
     protected String password;
-    private UserType userType;
+    private final UserType userType;
 
     public String getId() {
         return idUser;
