c13c6295ec66ae5b2524eb82f8b00c29131624f1
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-04-09 01:42:04 +0430

    add idRequest

--- untitled/src/model/for_user/request/Request.java
+++ untitled/src/model/for_user/request/Request.java
@@ -1,6 +1,13 @@
 package model.for_user.request;
 
 public abstract class Request {
-
+    public Request(){
+        this.idRequest = Integer.toHexString(3802+countForId++);
+    }
+    protected String idRequest;
+    private static int countForId;
+    static {
+        countForId = 0;
+    }
 }
 
