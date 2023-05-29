8effaabfa2e84cd47df0500beba8052b86a832dd
ardent83 <121909206+ardent83@users.noreply.github.com>
2023-05-29 08:07:07 +0430

    fix start

--- fist-project-phase2/src/main/java/com/example/fistprojectphase2/HelloApplication.java
+++ fist-project-phase2/src/main/java/com/example/fistprojectphase2/HelloApplication.java
@@ -2,19 +2,7 @@ package com.example.fistprojectphase2;
 
 import UI.MainPanel;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import model.item.food.Food;
-import model.user.Admin;
-import view.AdminPanel;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
